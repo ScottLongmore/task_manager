42a2bd7d8e4ab797f16752cf02c09d6d909ad4c3
Chris Slocum <cslocum@atmos.colostate.edu>
2015-08-31 20:22:38 +0000

    PEP8 libTask and added schema_completed.json

--- libTask.py
+++ libTask.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-libTask.py - library of routines for handling task...maybe come a module 
+libTask.py - library of routines for handling task...maybe come a module
 """
 
 # Stock modules
@@ -25,95 +25,113 @@ LOG = logging.getLogger(__name__)
 
 schema_completed = json.dump(schema_completed, open("./schema_completed.json", 'r'),
                              object_pairs_hook=collections.OrderedDict)
+
+
 def read_tasks_json_file(jsonFile):
-    '''Read Task list JSON file'''
+    """
+    Read Task list JSON file
+
+    Parameters
+    ----------
+    jsonFile : string
+        json file path
+
+    Returns
+    -------
+    tasks : dict
+        returns json dict if completed tasks
+    """
     try:
         LOG.info("Processing task list JSON file: {}".format(jsonFile))
         tasks = json.load(open(jsonFile, 'r'), object_pairs_hook=collections.OrderedDict)
-
         validator = jsonschema.Draft4Validator(schema_completed)
         errs = sorted(validator.iter_errors(tasks), key=lambda e: e.path)
-
         if errs:
-            msg=""
+            msg = ''
             for err in errs:
-                msg+=err.message
-            error(LOG,msg,error_codes.EX_IOERR)
-
+                msg += err.message
+            error(LOG, msg, error_codes.EX_IOERR)
     except:
         LOG.info("Unable to open/process: {} task list json file".format(jsonFile))
-        # CS -- What if there is no completed file? The return fails.
-        tasks = []
+        tasks = {}  # CS -- What if there is no completed file? The return fails.
     return(tasks)
 
-def write_tasks_json_file(jsonFile,tasks):
-    '''Write New Processed Image Files'''
 
+def write_tasks_json_file(jsonFile, tasks):
+    """
+    Write New Processed Image Files
+
+    Parameters
+    ----------
+    jsonFile : string
+        completed task json file path
+    tasks : dict
+
+    Returns
+    -------
+    None : NoneType
+    """
     try:
-        taskFH = open(jsonFile,"w")
-        taskFH.write(json.dumps(tasks,indent=2,skipkeys=True))
+        taskFH = open(jsonFile, "w")
+        taskFH.write(json.dumps(tasks, indent=4, skipkeys=True))
         taskFH.close()
     except:
-        msg="Unable to open/write: {} task list json file".format(jsonFile)
-        error(LOG,msg,error_codes.EX_IOERR)
-
+        msg = "Unable to open/write: {} task list json file".format(jsonFile)
+        error(LOG, msg, error_codes.EX_IOERR)
     return()
 
-def sort_tasks(tasks,keyOption,reverseOption):
+
+def sort_tasks(tasks, keyOption, reverseOption):
 
     try:
         sortedTasks = sorted(tasks, key=operator.itemgetter(keyOption), reverse=reverseOption)
     except:
-        msg="Unable to sort task list"
-        error(LOG,msg,error_codes.EX_IOERR)
-
+        msg = "Unable to sort task list"
+        error(LOG, msg, error_codes.EX_IOERR)
     return(sortedTasks)
 
-def compare_tasks(tasks1,tasks2):
+
+def compare_tasks(tasks1, tasks2):
 
     try:
         diffList = list(itertools.ifilterfalse(lambda x: x in tasks1, tasks2)) \
                  + list(itertools.ifilterfalse(lambda x: x in tasks2, tasks1))
     except:
-        msg="Unable to compare task lists"
-        error(LOG,msg,error_codes.EX_IOERR)
-
+        msg = "Unable to compare task lists"
+        error(LOG, msg, error_codes.EX_IOERR)
     return(diffList)
 
-def new_tasks(tasks,updatedTasks):
+
+def new_tasks(tasks, updatedTasks):
 
     try:
         newTasks = list(itertools.ifilterfalse(lambda x: x in tasks, updatedTasks))
-
     except:
-        msg="Unable to get new task lists"
-        error(LOG,msg,error_codes.EX_IOERR)
-
+        msg = "Unable to get new task lists"
+        error(LOG, msg, error_codes.EX_IOERR)
     return(newTasks)
 
-def prepend_tasks(tasks,prependTasks):
+
+def prepend_tasks(tasks, prependTasks):
 
     try:
         tasks[:0] = prependTasks
     except:
-        msg="Unable to prepend task lists"
-        error(LOG,msg,error_codes.EX_IOERR)
-
+        msg = "Unable to prepend task lists"
+        error(LOG, msg, error_codes.EX_IOERR)
     return(True)
+
 
 def print_tasks(tasks):
 
-    # pp = pprint.PrettyPrinter(indent=4,depth=20)
-    # pp.pprint(tasks)
- 
-    print json.dumps(tasks,indent=4)
-
+    print(json.dumps(tasks, indent=4))
+    sys.stdout.flush()
     return()
 
-def print_tasks_keys_values(tasks,key):
 
-    values=[]
+def print_tasks_keys_values(tasks, key):
+
+    values = []
     for task in tasks:
         values.append(task[key])
- 
     return(values)
